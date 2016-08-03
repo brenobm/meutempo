c632c7ce0416f58c4fb9ffa4bd5adfe11c809127
breno_cit <breno@ciandt.com>
2016-08-03 13:06:53 -0300

    Finalização do módulo 1

--- app/src/main/java/org/brenomachado/meutempo/MainActivity.java
+++ app/src/main/java/org/brenomachado/meutempo/MainActivity.java
@@ -2,6 +2,9 @@ package org.brenomachado.meutempo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] forecastList = {
+                "Hoje - Ensolarado - 24 / 18",
+                "Amanhã - Ensolarado - 24 / 18",
+                "Sex - Ensolarado - 24 / 18",
+                "Sab - Ensolarado - 24 / 18",
+                "Dom - Ensolarado - 24 / 18",
+                "Seg - Ensolarado - 24 / 18"
+        };
+
+        ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, forecastList);
+
+        ListView listView = (ListView) findViewById(R.id.listview_forecast);
+
+        listView.setAdapter(mForecastAdapter);
     }
+
+
 }
